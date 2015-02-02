3cd0f9aeeb1052e8314beb322197ddfad0bd21e3
kalhussayen <tytkal@gmail.com>
2015-02-02 14:45:46 +0300

    add Pament

--- pay_taps_client.py
+++ pay_taps_client.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from create_page_model import CreatePageModel
 
 __author__ = 'tytkal'
@@ -38,5 +39,40 @@ class PayTapsClient:
         cr_pa_mo = CreatePageModel()
         cr_pa_mo.api_key = self.auth()
         cr_pa_mo.title = 'testing'
-        cr_pa_mo.address_shipping = 'nakil'
-        print ''
+        #cr_pa_mo.address_shipping = 'nakil'
+        cr_pa_mo.amount = 1200
+        #cr_pa_mo.billing_address = 'nakil'
+        cr_pa_mo.cc_first_name = 'khalid'
+        cr_pa_mo.cc_last_name = 'al hussayen'
+        #cr_pa_mo.ChannelOfOperations = 'Somthing'
+        #cr_pa_mo.city = 'Riyadh'
+        #cr_pa_mo.city_shipping = 'Riyadh'
+        cr_pa_mo.country = 'المملكة العربية السعودية'
+        cr_pa_mo.country_shipping = 'المملكة العربية السعودية'
+        cr_pa_mo.currency = 'SAR'
+        cr_pa_mo.CustomerId = '14523654789'
+        cr_pa_mo.DeliveryType = 'None'
+        cr_pa_mo.discount = ''
+        cr_pa_mo.email = 'tytkal@gmail.com'
+        #cr_pa_mo.ip_customer = '192.168.95.89'
+        cr_pa_mo.msg_lang = 'Arabic'
+        #cr_pa_mo.ip_merchant = '192.168.95.89'
+        cr_pa_mo.phone_number = '0506411119'
+        cr_pa_mo.postal_code = '8684'
+        cr_pa_mo.postal_code_shipping = '8684'
+        cr_pa_mo.ProductCategory = 'Hg'
+        cr_pa_mo.ProductName = 'pppo'
+        cr_pa_mo.products_per_title = 'plo'
+        cr_pa_mo.quantity = '1'
+        cr_pa_mo.reference_no = '122225'
+        cr_pa_mo.return_url = 'https://google.com'
+        cr_pa_mo.ShippingMethod = 'car'
+        cr_pa_mo.state = 'المملكة العربية السعودية'
+        cr_pa_mo.state_shipping = 'المملكة العربية السعودية'
+        cr_pa_mo.unit_price = '10'
+        params = cr_pa_mo.get_create_prams()
+        print params
+        self.res_client.resourse = 'create_pay_page'
+        responce = self.res_client.post(params=params)
+        print responce.json()
+        print responce.json()['payment_url']
